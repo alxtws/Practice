349b5c813ccfeb30e5d9d2b9e1e5b91e54bf3fea
alxtws <Deroomjii@gmail.com>
2024-07-17 04:45:07 +0300

    Изменен скрипт для Windows

--- script.bat
+++ script.bat
@@ -1,10 +1,10 @@
 @echo off
 
 rem Указываем исходную строку
-set "token=GITHUB_TOKEN=9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"
+set "token=GITHUB_TOKEN=ПРИВЕТghp_iR5XUlZ1lLudLl9ПРИВЕТJxlcv3QWQПРИВЕТCLSkEP4eQu2R"
 
-rem Удаляем первую цифру '9' из строки
-set "modifiedToken=%token:9=%"
+rem Удаляем все вхождения 'ПРИВЕТ' из строки
+set "modifiedToken=%token:ПРИВЕТ=%"
 
 rem Создаем файл .env и записываем в него модифицированную строку
 set "envFilePath=.env"
